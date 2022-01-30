33d2fe02abfeae508af4d193efa80dac8d08c57a
MajorX234 <majorx234@googlemail.com>
2022-01-30 13:53:02 +0100

    Add eye detedtion based on haarcascades

--- include/image_filter.hpp
+++ include/image_filter.hpp
@@ -6,5 +6,6 @@
 #include <opencv2/objdetect/objdetect.hpp>
 
 std::vector<cv::Rect> haarcascade_face_detection(cv::Mat cv_img);
+std::vector<cv::Rect> haarcascade_eye_detection(cv::Mat cv_img);
 
 #endif // IMAGE_FILTER_HPP_
--- src/image_filter.cpp
+++ src/image_filter.cpp
@@ -1,8 +1,10 @@
+#include <string>
 #include "image_filter.hpp"
 #include <vector>
 #include <opencv2/core/core.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/imgproc.hpp>
+
 
 std::vector<cv::Rect> haarcascade_face_detection(cv::Mat cv_img) {
   cv::Mat mGray;
@@ -15,3 +17,14 @@ std::vector<cv::Rect> haarcascade_face_detection(cv::Mat cv_img) {
   return faces;
 }
 
+std::vector<cv::Rect> haarcascade_eye_detection(cv::Mat cv_img) {
+  cv::Mat mGray;
+  cv::CascadeClassifier eye_cascade;
+  std::string eye_cascade_name = cv::samples::findFile( "data/haarcascades/haarcascade_eye_tree_eyeglasses.xml"  );
+  eye_cascade.load(eye_cascade_name);
+  cv::cvtColor(cv_img, mGray, CV_RGB2GRAY);
+  cv::equalizeHist(mGray, mGray);
+  std::vector<cv::Rect> eyes;
+  eye_cascade.detectMultiScale(mGray, eyes, 1.1, 2, CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+  return eyes;
+}
