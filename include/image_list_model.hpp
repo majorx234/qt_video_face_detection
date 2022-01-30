bab5aced0377e22228590b3dafa4969baf63e4ce
MajorX234 <majorx234@googlemail.com>
2022-01-30 23:43:15 +0100

    Add flags function in model

--- include/image_list_model.hpp
+++ include/image_list_model.hpp
@@ -9,15 +9,16 @@ class ImageListModel : public QAbstractItemModel {
   Q_OBJECT
  public:
   explicit ImageListModel(QObject* parent = 0);
-  int rowCount(const QModelIndex& parent = QModelIndex()) const;
-  int columnCount(const QModelIndex& parent = QModelIndex()) const;
-  QVariant data(const QModelIndex &index, int role) const;
-  QModelIndex index(int, int, const QModelIndex&) const;
-  QModelIndex parent(const QModelIndex&) const;
-  bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+  int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+  int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+  QVariant data(const QModelIndex &index, int role) const override;
+  QModelIndex index(int, int, const QModelIndex&) const override;
+  QModelIndex parent(const QModelIndex&) const override;
+  bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
   void populate();
   void append(cv::Mat cv_image);
   std::vector<ImageItem*> list;
+  Qt::ItemFlags flags(const QModelIndex &index) const override;
  public slots: 
   void data_changed(ImageItem* item, const QModelIndex &index);
 };
--- src/image_list_model.cpp
+++ src/image_list_model.cpp
@@ -10,6 +10,8 @@ int ImageListModel::rowCount(const QModelIndex &parent) const {
 }
 
 int ImageListModel::columnCount(const QModelIndex &parent ) const {
+  if (list.size() > 0)
+    return 1;
   return 0;
 }
 
@@ -19,10 +21,11 @@ QVariant ImageListModel::data(const QModelIndex &index, int role) const {
   if ( role != Qt::DisplayRole )
     return QVariant();
   int list_index = index.row();
-  qDebug() << "ImageListodel::data: list_index: " << list_index;
+  qDebug() << "ImageListModel::data: list_index: " << list_index;
   ImageItem* image_item = list[list_index];
   QVariant v;
   v.setValue(image_item);
+  qDebug() << "ImageListModel::data2: data.type(): " << v.typeName();
   return v;  
 }
 
@@ -42,10 +45,16 @@ void ImageListModel::append(cv::Mat cv_image) {
   new_item->set_image(cv_image);
   // connect(new_item,SIGNAL(valueChanged()),SLOT(dataChanged()));
   list.push_back(new_item);
-  int list_index = list.size() - 1;
-  qDebug() << "ImageListodel::data: list_append: " << list_index;
+  int list_index = list.size();
+  //beginInsertRows(QModelIndex(), list_index, list_index);
+  qDebug() << "ImageListModel::append: list_append: " << list_index;
   QModelIndex index = createIndex(list_index, 0, new_item);
-  emit dataChanged(index,index, {Qt::EditRole});
+  emit dataChanged(index, index, {Qt::EditRole});
+  qDebug() << "ImageListModel::append2: index.type(): " << index.data().typeName();
+  int row = index.row();
+  int column = index.column();
+  qDebug() << "append: row " << row << " column: " << column;  
+  //endInsertRows();
 }
 
 void ImageListModel::data_changed(ImageItem* item, const QModelIndex &index) {
@@ -60,4 +69,11 @@ bool ImageListModel::setData(const QModelIndex &index, const QVariant &value, in
   ImageItem* item = data.value<ImageItem*>();
   emit dataChanged(index, index, {Qt::EditRole});
   return true;
+}
+
+Qt::ItemFlags ImageListModel::flags(const QModelIndex &index) const
+{
+    if (!index.isValid())
+        return {};
+    return Qt::ItemIsDragEnabled|QAbstractItemModel::flags(index);
 }
