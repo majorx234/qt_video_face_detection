99d426f1400ecc3904ab6973323caca64631ba09
majorx234 <majorx234@googlemail.com>
2023-09-11 18:26:46 +0200

    Add guards to prevent crashing caused by empty cv_img

--- src/image_filter.cpp
+++ src/image_filter.cpp
@@ -7,36 +7,42 @@
 #include "detect_multiscale_param.hpp"
 
 std::vector<cv::Rect> haarcascade_face_detection(cv::Mat cv_img, DetectMultiscaleParam params) {
-  cv::Mat mGray;
-  cv::CascadeClassifier face_cascade;
-  std::string face_cascade_name = cv::samples::findFile( "haarcascades/haarcascade_frontalface_default.xml",true,true);
-  face_cascade.load(face_cascade_name);
-  cv::cvtColor(cv_img, mGray, cv::COLOR_BGR2GRAY);
-  cv::equalizeHist(mGray, mGray);
   std::vector<cv::Rect> faces;
-  face_cascade.detectMultiScale(mGray, faces, params.scaleFactor, params.minNeighbors, params.flags, params.minSize);
+  if(!cv_img.empty()){
+    std::string face_cascade_name = cv::samples::findFile( "haarcascades/haarcascade_frontalface_default.xml",true,true);
+    cv::CascadeClassifier face_cascade;
+    face_cascade.load(face_cascade_name);
+    cv::Mat mGray;
+    cv::cvtColor(cv_img, mGray, cv::COLOR_BGR2GRAY);
+    cv::equalizeHist(mGray, mGray);
+    face_cascade.detectMultiScale(mGray, faces, params.scaleFactor, params.minNeighbors, params.flags, params.minSize);
+  }
   return faces;
 }
 
 std::vector<cv::Rect> haarcascade_eye_detection(cv::Mat cv_img, DetectMultiscaleParam params) {
-  cv::Mat mGray;
-  cv::CascadeClassifier eye_cascade;
-  std::string eye_cascade_name = cv::samples::findFile( "haarcascades/haarcascade_eye_tree_eyeglasses.xml",true,true );
-  eye_cascade.load(eye_cascade_name);
-  cv::cvtColor(cv_img, mGray, cv::COLOR_BGR2GRAY);
-  cv::equalizeHist(mGray, mGray);
   std::vector<cv::Rect> eyes;
-  eye_cascade.detectMultiScale(mGray, eyes, params.scaleFactor, params.minNeighbors, params.flags, params.minSize);
+  if(!cv_img.empty()){
+    cv::Mat mGray;
+    cv::CascadeClassifier eye_cascade;
+    std::string eye_cascade_name = cv::samples::findFile( "haarcascades/haarcascade_eye_tree_eyeglasses.xml",true,true );
+    eye_cascade.load(eye_cascade_name);
+    cv::cvtColor(cv_img, mGray, cv::COLOR_BGR2GRAY);
+    cv::equalizeHist(mGray, mGray);
+    eye_cascade.detectMultiScale(mGray, eyes, params.scaleFactor, params.minNeighbors, params.flags, params.minSize);
+  }
   return eyes;
 }
 
-void pixelate_image(cv::Mat &cv_image, std::vector<cv::Rect> rectangle_list) {
-  for (cv::Rect &rectangle : rectangle_list) {
-    cv::Mat face_image = cv_image(rectangle);
-    cv::resize(face_image, face_image, cv::Size(), 0.1, 0.1, cv::INTER_NEAREST);
-    cv::resize(face_image, face_image, cv::Size(), 10.0, 10.0, cv::INTER_NEAREST);
+void pixelate_image(cv::Mat &cv_img, std::vector<cv::Rect> rectangle_list) {
+  if(!cv_img.empty()) {
+    for (cv::Rect rectangle : rectangle_list) {
+      cv::Mat face_image = cv_img(rectangle);
+      cv::resize(face_image, face_image, cv::Size(), 0.1, 0.1, cv::INTER_NEAREST);
+      cv::resize(face_image, face_image, cv::Size(), 10.0, 10.0, cv::INTER_NEAREST);
 
-    cv::Mat insert_image(cv_image, rectangle);
-    face_image.copyTo(insert_image);
+      cv::Mat insert_image(cv_img, rectangle);
+      face_image.copyTo(insert_image);
+    }
   }
 }
--- src/image_item.cpp
+++ src/image_item.cpp
@@ -3,11 +3,13 @@
 ImageItem::ImageItem(unsigned int thumb_width) : thumbnail_width(thumb_width) {
 }
 
-void ImageItem::set_image(cv::Mat cv_image) {
-  this->image = cv_image;
-  cv::Size image_size = cv_image.size();
-  double ratio = static_cast<double>(image_size.width) / static_cast<double>(image_size.height);
-  cv::resize(cv_image, this->thumbnail, cv::Size(128,static_cast<int>(128 / ratio)), 0, 0, cv::INTER_CUBIC);
+void ImageItem::set_image(cv::Mat cv_img) {
+  if(!cv_img.empty()){
+    this->image = cv_img;
+    cv::Size image_size = cv_img.size();
+    double ratio = static_cast<double>(image_size.width) / static_cast<double>(image_size.height);
+    cv::resize(cv_img, this->thumbnail, cv::Size(128,static_cast<int>(128 / ratio)), 0, 0, cv::INTER_CUBIC);
+  }
 }
 
 cv::Mat ImageItem::get_image() {
